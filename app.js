ba83848fd85a649110360ffc42e4a3a97280c05d
Eric Smith <esmith@8thlight.com>
2016-01-23 09:38:57 -0600

    App is server, server is app

--- app.js
+++ app.js
@@ -1,69 +1,19 @@
-"use strict"
+var http = require("http");
+var server;
 
-var server = require("./server");
-server.start();
-
-/*var express = require('express');
-var methodOverride = require('method-override');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var tasks = require('./routes/tasks');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(methodOverride('_method'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/users/:userId/tasks', tasks);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+module.exports = {
+  start: function(port) {
+    server = http.createServer(function(request, response) {
+      response.writeHead(302, {
+        'Location': '/users/'
+      });
+      response.end();
     });
-  });
-}
+    server.listen(port);
+  },
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+  stop: function() {
+    server.close();
+  }
+};
 
-
-module.exports = app;*/
--- test/routes/index_test.js
+++ test/routes/index_test.js
@@ -2,20 +2,9 @@ describe("Index page - routes right to users routes", function() {
   var http = require('http');
   var expect = require('expect.js');
   var app = require('../../server.js');
-//  var server = require("../../server.js");
-  var server;
 
   before(function() {
     app.start(8888);
-    /*
-     *server = http.createServer(function(request, response) {
-     *  response.writeHead(302, {
-     *    'Location': '/users/'
-     *  });
-     *  response.end();
-     *});
-     *server.listen(8888);
-     */
   });
 
   after(function() {
