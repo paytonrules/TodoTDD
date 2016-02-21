c97bc3f9196537daa81cb731b7ef2c3474065955
Eric Smith <esmith@8thlight.com>
2016-02-21 11:24:12 -0600

    Remove express framework

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var models = require("../models");
-
-router.get('/', function(req, res, next) {
-  res.redirect('/users');
-});
-
-module.exports = router;
